4c8af45657c4dd3de44dc14c8ffedc3ace8895d7
DeveloperYamin <developer.yamin@gmail.com>
2022-01-11 11:53:57 +0600

    fix some problems

--- components/Nav.js
+++ components/Nav.js
@@ -6,9 +6,9 @@ function Nav() {
      const router = useRouter();
      return (
           <nav className="relative">
-               <div className='flex px-10 pb-2 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
+               <div className='flex px-10 pb-2 space-x-10 overflow-x-scroll text-2xl sm:px-20 whitespace-nowrap sm:space-x-20 scrollbar-hide'>
                     {Object.entries(requests).map(([key, {title,url}]) => (
-                         <h2 key={key} onClick={() => router.push(`/?genre=${key}`)} className="cursor-pointer last:pr-24 transition transform duration-100 hover:scale-125 hover:text-white active:text-red-500">{title}</h2>
+                         <h2 key={key} onClick={() => router.push(`/?genre=${url}`)} className="transition duration-100 transform cursor-pointer last:pr-24 hover:scale-125 hover:text-white active:text-red-500">{title}</h2>
                     ))}
                </div>
                <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
